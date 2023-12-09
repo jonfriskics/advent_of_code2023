f6097da8acce60e227f71091180517c837beb159
Jon Friskics <jonfriskics@gmail.com>
2023-12-09 00:12:29 -0500

    day 8 part 2

--- day08.py
+++ day08.py
@@ -1,5 +1,6 @@
 import os
 import re
+import math
 
 input = ''
 
@@ -26,6 +27,19 @@ with open(f'inputs/input{day}.txt', 'r') as file:
 # AAA = (BBB, BBB)
 # BBB = (AAA, ZZZ)
 # ZZZ = (ZZZ, ZZZ)
+# """
+
+# input = """
+# LR
+
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)
 # """
 
 star1 = 0
@@ -58,6 +72,33 @@ for inst in inst_repeating:
     steps += 1
 
 # star 2
+
+start1 = 'HVA'
+start2 = 'HHA'
+start3 = 'BVA'
+start4 = 'RSA'
+start5 = 'AAA'
+start6 = 'NPA'
+
+def count_steps(start, network_d, instructions_list):
+    steps = 0
+    i = 0
+    while 1 == 1:
+        if start[-1] == 'Z':
+            return steps
+        elif instructions_list[i] == 'L':
+            start = network_d[start][0]
+        elif instructions_list[i] == 'R':
+            start = network_d[start][1]
+        steps += 1
+        if i == len(instructions_list)-1:
+            i = 0
+        else:
+            i += 1
+
+instructions_list = list(instructions)
+
+star2 = math.lcm(count_steps(start1, network_d, instructions_list), count_steps(start2, network_d, instructions_list), count_steps(start3, network_d, instructions_list), count_steps(start4, network_d, instructions_list), count_steps(start5, network_d, instructions_list), count_steps(start6, network_d, instructions_list))
 
 print(f"star 1: {star1}")
 print(f"star 2: {star2}")
